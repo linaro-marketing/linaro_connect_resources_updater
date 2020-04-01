1abe610ee7602a6849fc286b94a3542b44348b7a
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-04-01 11:39:08 +0100

    Example script updated

--- examples/test.py
+++ examples/test.py
@@ -9,11 +9,11 @@ from secret import SCHED_API_KEY
 
 if __name__ == "__main__":
     sched_data_interface = SchedDataInterface(
-        "https://bud20.sched.com", SCHED_API_KEY, "BUD20")
+        "https://linarotechdays.sched.com", SCHED_API_KEY, "LTD20")
     sched_data = sched_data_interface.getSessionsData()
     json_updater = ConnectJSONUpdater(
-        "static-linaro-org", "connect/bud20/", sched_data, "")
-
+        "static-linaro-org", "connect/ltd20/", sched_data, "output/")
+    # json_updater.create_initial_resources_json_file()
     missing_presentations = json_updater.getMissingPresentations()
     with open("missing_presentations.txt", "w") as my_file:
         for each in missing_presentations:
